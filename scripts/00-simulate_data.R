0b355fab5576e4d9cc0790d9e4912e6a5fbfab3a
Alexander Sun <boyalexsun@gmail.com>
2024-03-18 05:22:56 -0400

    name added

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -14,7 +14,7 @@ library(tidyverse)
 set.seed(1111)
 alberta_death_simulation <-
   tibble(
-    cause = rep(x = c("Heart", "Stroke", "Diabetes"), times = 10),
+    cause = rep(x = c("Heart Attack", "Heart Disease", "Diabetes"), times = 10),
     year = rep(x = 2016:2018, times = 10),
     deaths = rnbinom(n = 30, size = 20, prob = 0.1)
   )
@@ -23,5 +23,9 @@ alberta_death_simulation
 summary(alberta_death_simulation)
 write_csv(alberta_death_simulation, "data/raw_data/simulated_data.csv") 
 
-
-
+#Test for deaths being at least 0
+alberta_death_simulation$deaths |> min() >= 0 
+#Test for all causes being in the data set
+all(c("Heart Attack", "Heart Disease", "Diabetes") %in% alberta_death_simulation$cause) 
+#Test so that death count doesn't exceed reasonable values
+lberta_death_simulation$deaths |> max() <= 20000
